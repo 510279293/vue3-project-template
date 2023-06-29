6b2d78b694af8e53b59fadcd16d7c2c5208ba1aa
zeng <510279293@qq.com>
2023-06-29 15:10:32 +0800

    add reset.less

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,20 +4,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 const HomeView = () => import('@/views/HomeView.vue')
 const About = () => import('@/views/AboutView.vue')
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+  routes
 })
 
 export default router
