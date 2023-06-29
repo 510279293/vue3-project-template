9a6672445db59ce3bfe6c34239fce5149947d9f6
zeng <510279293@qq.com>
2023-06-29 16:01:16 +0800

    test: 测试一下

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -15,15 +15,15 @@ const types = [
     'revert', // 回滚某个更早的提交
     'style', // 不影响程序逻辑的代码修改、主要是样式方面的优化、修改
     'test', // 测试相关的开发,
-  ],
-  typeEnum = {
-    rules: {
-      'type-enum': [2, 'always', types],
-    },
-    value: () => {
-      return types;
-    },
-  };
+],
+typeEnum = {
+  rules: {
+    'type-enum': [2, 'always', types],
+  },
+  value: () => {
+    return types;
+  },
+}
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
